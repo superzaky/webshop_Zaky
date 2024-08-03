0f1d161d20d4ccec7f233168fcf6f2fe237b27cb
superzaky <superkytoz@gmail.com>
2024-08-03 22:43:33 +0200

    wip: fixing invalid hook call

--- webshop_sample/src/admin/routes/reviews/page.tsx
+++ webshop_sample/src/admin/routes/reviews/page.tsx
@@ -1,29 +1,94 @@
 import { defineRouteConfig } from "@medusajs/admin-shared"
 import { ChatBubbleLeftRight } from "@medusajs/icons"
-import { Container, Table  } from "@medusajs/ui"
+import { Container, Heading, Table, Button, Drawer } from "@medusajs/ui"
+import { useEffect, useMemo, useState } from "react"
+import { Link } from "react-router-dom"
 
-let qwe = 'asd'
+const [reviews, setReviews] = useState([]);
+
+const [open, setOpen] = useState(false)
+  
+const [currentPage, setCurrentPage] = useState(0)
+const pageLimit = 10
+const [count, setCount] = useState(0)
+const pagesCount = useMemo(() => {
+  return count / pageLimit
+}, [count])
+const canNextPage = useMemo(
+  () => currentPage < pagesCount - 1, 
+  [currentPage, pagesCount]
+)
+const canPreviousPage = useMemo(
+  () => currentPage > 0, 
+  [currentPage]
+)
+
+const nextPage = () => {
+  if (canNextPage) {
+    setCurrentPage((prev) => prev + 1)
+  }
+}
+
+const previousPage = () => {
+  if (canPreviousPage) {
+    setCurrentPage((prev) => prev)
+  }
+}
+
+const fetchProducts = () => {
+  const query = new URLSearchParams({
+    limit: `${pageLimit}`,
+    offset: `${pageLimit * currentPage}`
+  })
+  
+  fetch(`/store/review?${query.toString()}`, {
+    credentials: "omit"
+  })
+  .then((res) => res.json())
+  .then(({ 
+    digital_products: data, 
+    count
+  }) => {
+    setReviews(data)
+    setCount(count)
+  })
+}
+
 const CustomPage = () => {
-  return <Container>This is my custom route {qwe}
+  return <Container>This is my custom route
   
   <Table>
-  <Table.Header>
-    <Table.Row>
-      <Table.HeaderCell>#</Table.HeaderCell>
-      <Table.HeaderCell>Customer</Table.HeaderCell>
-      <Table.HeaderCell>Email</Table.HeaderCell>
-    </Table.Row>
-  </Table.Header>
-  <Table.Body>
-    <Table.Row>
-      <Table.Cell>1</Table.Cell>
-      <Table.Cell>Emil Larsson</Table.Cell>
-      <Table.Cell>emil2738@gmail.com</Table.Cell>
-    </Table.Row>
-  </Table.Body>
-</Table>
-
-
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Name</Table.HeaderCell>
+            <Table.HeaderCell>Action</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {reviews.map((review) => (
+            <Table.Row key={review.id}>
+              <Table.Cell>
+                {review.title}
+              </Table.Cell>
+              <Table.Cell>
+                <Link to={`/products/${review.id}`}>
+                  View Product
+                </Link>
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+      <Table.Pagination
+        count={count}
+        pageSize={pageLimit}
+        pageIndex={currentPage}
+        pageCount={pagesCount}
+        canPreviousPage={canPreviousPage}
+        canNextPage={canNextPage}
+        previousPage={previousPage}
+        nextPage={nextPage}
+      />
   </Container>
 }
 
---
+++ webshop_sample/src/api/admin/review/route.ts
@@ -0,0 +1,53 @@
+import { AuthenticatedMedusaRequest, MedusaRequest, MedusaResponse } from "@medusajs/medusa"
+import ReviewModuleService from "../../../modules/review/review_service"
+import { REVIEW_MODULE } from "../../../modules/review"
+import { remoteQueryObjectFromString, ContainerRegistrationKeys } from "@medusajs/utils"
+import type { RemoteQueryFunction } from "@medusajs/modules-sdk"
+
+/**
+ * Onderstaande API kan niet gebruikt worden voor een bepaalde reden desondanks een gebruiker een Bearer token stuurt..
+ */
+export async function POST(
+  req: MedusaRequest,
+  res: MedusaResponse
+): Promise<void> {
+  const reviewModuleService: ReviewModuleService = req.scope.resolve(
+    REVIEW_MODULE
+  )
+  const review = await reviewModuleService.createReviews(req.body)
+  res.json({
+    message: review
+  })
+}
+
+export async function GET(
+  req: AuthenticatedMedusaRequest,
+  res: MedusaResponse
+): Promise<void> {
+  const remoteQuery: RemoteQueryFunction = req.scope.resolve(
+    ContainerRegistrationKeys.REMOTE_QUERY
+  )
+
+  let page = 0;
+  if (req.query['offset'] !== undefined || req.query['offset'] !== '0') {
+      page = Number(req.query['offset']) - 1
+  }
+
+  // per page 
+  const perPage = Number(req.query['limit'])
+  // per page * page 
+  const result = perPage * page
+
+  const query = remoteQueryObjectFromString({
+    entryPoint: "review",
+    fields: ["*"],
+    variables: {
+      skip: result,
+      take: perPage,
+    }
+  })
+
+  res.json({
+    my_customs: await remoteQuery(query),
+  })
+}
